c8db4e5ff1f5534407099daccb05c93b624fdfca
Liam Dale <liamdale15@gmail.com>
2019-03-19 16:18:13 +1000

    Initial commit

---
+++ UnitTesting/UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MedianAlgorithm;
+
+namespace MedianTests
+{
+    [TestClass]
+    public class ValueTests
+    {
+        [TestMethod]
+        public void basicMedian()
+        {
+            int[] simpleMedian = new int[] { 1, 2, 3, 4, 5 };
+            int output = Median.BruteForceMedian(simpleMedian);
+            int expected = 3;
+
+
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
