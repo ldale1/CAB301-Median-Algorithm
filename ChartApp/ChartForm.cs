cd80957750fb97083e6f25c7c2899e224851b095
Liam Dale <liamdale15@gmail.com>
2019-04-09 21:42:42 +1000

    test

--- ChartApp/ChartForm.cs
+++ ChartApp/ChartForm.cs
@@ -40,18 +40,33 @@ namespace ChartApp
             EfficiencyChart.Titles.Add(chtTitle);
 
             // Add some series to the chart
-            var series = new Series("Basic");
+            var series = new Series("y1");
             series.ChartType = SeriesChartType.Point;
             EfficiencyChart.Series.Add(series);
 
             // Plot the output of time points
-            series = new Series("Time");
+            series = new Series("y2");
             series.ChartType = SeriesChartType.Point;
             series.YAxisType = AxisType.Secondary;
             EfficiencyChart.Series.Add(series);
 
             // Add a line of best fit series
-            series = new Series("Best Fit");
+            series = new Series("y1-oh");
+            series.ChartType = SeriesChartType.Spline;
+            EfficiencyChart.Series.Add(series);
+
+            // Add a line of best fit series
+            series = new Series("y1-omega");
+            series.ChartType = SeriesChartType.Spline;
+            EfficiencyChart.Series.Add(series);
+
+            // Add a line of best fit series
+            series = new Series("y2-oh");
+            series.ChartType = SeriesChartType.Spline;
+            EfficiencyChart.Series.Add(series);
+
+            // Add a line of best fit series
+            series = new Series("y2-omega");
             series.ChartType = SeriesChartType.Spline;
             EfficiencyChart.Series.Add(series);
 
@@ -80,34 +95,47 @@ namespace ChartApp
         }
         
         // Put on a series or take it off
-        private void series_on(Series series, int axis = 1)
+        private void series_toggle(Series series, bool onoff)
         {
-            series.IsVisibleInLegend = true;
-            int axisType = (axis == 1) ? 0 : 1;
-            series.YAxisType = (AxisType) axisType;
-        }
-        private void series_off(Series series) {
-            series.IsVisibleInLegend = false;
-            series.Points.Clear();
+            series.IsVisibleInLegend = onoff;
+            if (!onoff) { series.Points.Clear(); }
         }
 
 
         // Upper bound
-        private double[] bestfit_oh(double[] y)
+        private double[] bestfit_oh(double[] x_series)
         {
-            //x_series.Select(x => 0.5 * x * x).ToArray()
-            return new double[0];
+            double g = 0.0;
+            int breakCounter = 100;
+            double[] vals;
+            bool tuning;
+            do {
+                vals = Enumerable.Range(1, x_series.Length).Select(x => g * x * x + 1).ToArray();
+                tuning = vals.Where(val => val < x_series[Array.IndexOf(vals, val)]).Any(); // While any point < x_series
+                // Loop updating
+                g += 0.025;
+                breakCounter--;
+            } while (tuning && breakCounter > 0) ;
+            return vals;
         }
         // Lower bound
-        private double[] bestfit_omega(double[] y)
+        private double[] bestfit_omega(double[] x_series)
         {
-            return new double[0];
+            double g = 0.65;
+            int breakCounter = 100;
+            double[] vals;
+            bool tuning;
+            do
+            {
+                vals = Enumerable.Range(1, x_series.Length).Select(x => g * x * x - 1).ToArray();
+                tuning = vals.Where(val => val > x_series[Array.IndexOf(vals, val)]).Any(); // While any point > x_series
+                // Loop updating
+                g -= 0.025;
+                breakCounter--;
+            } while (tuning && breakCounter > 0);
+            return vals;
         }
-        // Middle
-        private double[] bestfit_theta(double[] y)
-        {
-            return new double[0];
-        }
+
 
         private void update_chart(object sender, EventArgs e)
         {
@@ -124,60 +152,55 @@ namespace ChartApp
                 EfficiencyChart.Titles["MyTitle"].Text = "Size vs Basic Ops and Time";
                 chartArea.AxisY.Title = "Count";
                 chartArea.AxisY2.Title = "Time (ms)";
-                series_on(EfficiencyChart.Series["Basic"]);
-                series_on(EfficiencyChart.Series["Time"], 2);
+
+                // Enable secondary
+                series_toggle(EfficiencyChart.Series["y2"], true);
+                series_toggle(EfficiencyChart.Series["y2-oh"], true);
+                series_toggle(EfficiencyChart.Series["y2-omega"], true);
 
                 // Plot the output
                 double[] y_basic = (from size in Enumerable.Range(1, problemSize)
-                                    select MedianOps.countBasics(size, iterations)
-                                    ).ToArray();
+                                    select MedianOps.countBasics(size, iterations)).ToArray();
+                EfficiencyChart.Series["y1"].Points.DataBindXY(x_series, y_basic);
+
+                // Plot the output for time
                 double[] y_time = (from size in Enumerable.Range(1, problemSize)
-                                   select MedianOps.countTime(size, iterations)
-                                   ).ToArray();
-                EfficiencyChart.Series["Basic"].Points.DataBindXY(x_series, y_basic);
-                EfficiencyChart.Series["Time"].Points.DataBindXY(x_series, y_time);
-            } else if (chartCombo.SelectedIndex == (int)ComboEnum.BASIC)
-            {
-                // Formatting
-                EfficiencyChart.Titles["MyTitle"].Text = "Size vs Basic Operations";
-                chartArea.AxisY.Title = "Count";
-                series_on(EfficiencyChart.Series["Basic"]);
-                series_off(EfficiencyChart.Series["Time"]);
-
-                // Plot the output
-                double[] y_basic = (from size in Enumerable.Range(1, problemSize)
-                                    select MedianOps.countBasics(size, iterations)
-                                    ).ToArray();
-                EfficiencyChart.Series["Basic"].Points.DataBindXY(x_series, y_basic);
-
-
-                try
-                {
-                    // Get a line of best fit
-                    double[] y_bf = bestfit_oh(y_basic);
-                    EfficiencyChart.Series["Best Fit"].Points.DataBindXY(x_series, y_bf);
-                }
-                catch (Exception exc)
-                {
-                    Console.WriteLine(exc);
-                }
-            } else
-            {
-                // Formatting
-                EfficiencyChart.Titles["MyTitle"].Text = "Size vs Time";
-                chartArea.AxisY.Title = "Time (ms)";
-                series_off(EfficiencyChart.Series["Basic"]);
-                series_on(EfficiencyChart.Series["Time"]);
-
-                // Plot the output
-                double[] y_time = (from size in Enumerable.Range(1, problemSize)
-                                   select MedianOps.countTime(size, iterations)
-                                   ).ToArray();
-                EfficiencyChart.Series["Time"].Points.DataBindXY(x_series, y_time);
+                                   select MedianOps.countTime(size, iterations)).ToArray();
+                EfficiencyChart.Series["y2"].Points.DataBindXY(x_series, y_time);
             }
+            else
+            {
+                // Labelling
+                String subtitle = (chartCombo.SelectedIndex == (int)ComboEnum.BASIC) ? "Basic Ops" : "Time";
+                EfficiencyChart.Titles["MyTitle"].Text = String.Format("Size vs {0}", subtitle);
+                String axistitle = (chartCombo.SelectedIndex == (int)ComboEnum.BASIC) ? "Count" : "Time [ms]";
+                chartArea.AxisY.Title = axistitle;
 
+                // Disable secondary
+                EfficiencyChart.Series["y1"].LegendText = subtitle;
+                EfficiencyChart.Series["y1-oh"].LegendText = String.Format("O({0})", subtitle);
+                EfficiencyChart.Series["y1-omega"].LegendText = String.Format("Ω({0})", subtitle);
 
-            series_off(EfficiencyChart.Series["Best Fit"]);
+                // Toggle off the secondary axis
+                series_toggle(EfficiencyChart.Series["y2"], false);
+                series_toggle(EfficiencyChart.Series["y2-oh"], false);
+                series_toggle(EfficiencyChart.Series["y2-omega"], false);
+
+                EfficiencyChart.Series["y1-oh"].BorderWidth = 5;
+                EfficiencyChart.Series["y1-omega"].BorderWidth = 5;
+
+                // Plot the series
+                double[] y1 = (chartCombo.SelectedIndex == (int)ComboEnum.BASIC) ?
+                     (from size in Enumerable.Range(1, problemSize) select MedianOps.countBasics(size, iterations)).ToArray() :
+                     (from size in Enumerable.Range(1, problemSize) select MedianOps.countTime(size, iterations)).ToArray();
+                EfficiencyChart.Series["y1"].Points.DataBindXY(x_series, y1);
+
+                // Bestfit fit
+                double[] y1_oh = bestfit_oh(y1);
+                double[] y1_omega = bestfit_omega(y1);
+                //EfficiencyChart.Series["y1-oh"].Points.DataBindXY(x_series, y1_oh);
+                //EfficiencyChart.Series["y1-omega"].Points.DataBindXY(x_series, y1_omega);
+            }
         }
 
         private void save_chart(object sender, EventArgs e)
--- MedianAlgorithm/Program.cs
+++ MedianAlgorithm/Program.cs
@@ -29,25 +29,37 @@ namespace MedianAlgorithm
         public static double countTime(int size, int iterations)
         {
             DateTime start, end;
-            double executionTime = 0;
-            double[] A;
+            
+            
 
             int randUpper = (int)Math.Floor(size / 2.0);
             int randLower = -randUpper;
-
+            double output;
+            double[] A;
+            double executionTime = 0;
             for (int i = 0; i < iterations; i++)
             {
                 A = randomArray(size, randLower, randUpper);
 
                 // Time over algorithm
                 start = DateTime.Now;
-                Median.BruteForceMedian(A);
+                output = Median.BruteForceMedian(A);
                 end = DateTime.Now;
 
                 // Aggreggate Time
                 executionTime += (end - start).TotalMilliseconds / iterations;
+
             }
             return executionTime;
+
+            // Time over algorithm for a number of iterations
+            start = DateTime.Now;
+            for (int i = 0; i < iterations; i++)
+            {
+                output = Median.BruteForceMedian(randomArray(size, randLower, randUpper));
+            }
+            end = DateTime.Now;
+            return (end - start).Ticks / (10 * iterations);
         }
 
         static void Main(string[] args)
@@ -55,10 +67,10 @@ namespace MedianAlgorithm
             double[] A = Enumerable.Range(1, 10).ToArray().Select(x => (double)x).ToArray();
 
             Console.WriteLine("TIMED:");
-            Console.WriteLine("{0} [ms]\n", countTime(1000, 1000));
+            Console.WriteLine("{0} [ms]\n", countTime(20, 250));
 
             Console.WriteLine("BASICS:");
-            Console.WriteLine("{0} [ops]", countBasics(1000, 1000));
+            Console.WriteLine("{0} [ops]\n", countBasics(100, 100));
 
             Console.WriteLine();
             Console.ReadLine();
